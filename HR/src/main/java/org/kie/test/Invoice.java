f2b6aaadb5b17360e3b2d96465c13d4a444eea25
guvnorngtestuser1 <guvnorngtestuser1@gmail.com>
2013-06-17 16:56:50 +0200

    Create Invoice.java

---
+++ HR/src/main/java/org/kie/test/Invoice.java
@@ -0,0 +1,29 @@
+package org.kie.test;
+
+public class Invoice implements java.io.Serializable {
+
+    static final long serialVersionUID = 1L;
+
+    private String number;
+    
+    private String client;
+
+    public Invoice() {
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getClient() {
+        return client;
+    }
+
+    public void setClient(String client) {
+        this.client = client;
+    }
+}
