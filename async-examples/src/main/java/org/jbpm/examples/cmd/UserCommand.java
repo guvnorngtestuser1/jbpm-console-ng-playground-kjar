23d730c21db4963cf0d5838277c62e9a4842955b
guvnorngtestuser1 <guvnorngtestuser1@gmail.com>
2015-08-01 07:19:25 +0200

    updated imports after promoting jbpm executor to public api

--- async-examples/src/main/java/org/jbpm/examples/cmd/UserCommand.java
+++ async-examples/src/main/java/org/jbpm/examples/cmd/UserCommand.java
@@ -17,9 +17,9 @@
 package org.jbpm.examples.cmd;
 
 import org.kie.api.runtime.process.WorkItem;
-import org.kie.internal.executor.api.Command;
-import org.kie.internal.executor.api.CommandContext;
-import org.kie.internal.executor.api.ExecutionResults;
+import org.kie.api.executor.Command;
+import org.kie.api.executor.CommandContext;
+import org.kie.api.executor.ExecutionResults;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
